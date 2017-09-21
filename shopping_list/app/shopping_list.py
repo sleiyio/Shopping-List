65223ccf2b8f1e5e100bb3e522b104920fc7d067
sleiyio <30837553+sleiyio@users.noreply.github.com>
2017-09-21 13:25:46 +0300

    Add files via upload

--- shopping_list/app/shopping_list.py
+++ shopping_list/app/shopping_list.py
@@ -1,47 +1,37 @@
 
-from flask import Flask, render_template, json, request
-app = Flask(__name__)
+class shoppingList(object):
 
-#default home page - index.html
-@app.route('/')
-def main():
-    return render_template('index.html')
+    def __init__(self, userId = 1, numberOfItems = 0, items = {}, totalCost = 0):
+        self.userId = userId
+        self.numberOfItems = 0
+        self.items = {}
+        self.totalCost = totalCost
 
-#Log in page
-@app.route('/index')
-def showIndex():
-    return render_template('index.html')
+    def addItem(self, item, quantity):
+        #Add new item to shopping list
+        self.items[item] = quantity
+        self.numberOfItems = self.numberOfItems + 1
 
-#New user to create create account
-@app.route('/createAccount')
-def showSignUp():
-    return render_template('new_account.html')
+    def removeItem(self, item):
+        #Delete an item from the shopping list
+        del(self.items[item])
+        self.numberOfItems = self.numberOfItems - 1
 
-#Create account once user clicks "Create Account" button
-@app.route('/newAccount', methods=['POST'])
-def newAccount(): 
-    
-    # get the values keyed in by user
-    firstname = "John"#request.form['firstName']
-    lastname = "Doe"#request.form['lastName']
-    address = "123"#request.form['address']
-    email = "johndoe@gmail.com"#request.form['email']
-    phone = "0722698553"#request.form['phone']
-    password = "xyz"#request.form['password']
-    repeatpassword = "xyz"#request.form['repeat_password']
-       
-     # validate the user input
-    if firstname and lastname and address and email and phone and password and repeatpassword:
-        #return json.dumps({'html':'<span>All fields have been completed. Ready to create user !!</span>'})
-        return render_template('index.html')
-    else:
-        return json.dumps({'html':'<span>Please complete all the fields</span>'})
+    def editItem(self, item, quantity):
+        #Edit quantity of items on shopping list
+        self.items[item] = quantity
+
+    def ViewShoppingList(self):
+        #View the whole shopping list
+        return self.items
+
+    def no_OfItems(self):
+        return self.numberOfItems
+
+    def quantityOfItems(self, item):
+        return self.items[item]
+
     
 
-#Dashboard page
-@app.route('/dashboard')
-def showDashboard():
-    return render_template('dashboard.html')
 
-if __name__ == "__main__":
-    app.run()
+    
