3c8903dae5f8b9cd463b76e2a649d668295c033b
unknown <sleiyio@gmail.com>
2017-09-28 14:44:17 +0300

    ft #151469426 html & css for the dashboard page

--- shopping_list/app/user.py
+++ shopping_list/app/user.py
@@ -1,45 +1,25 @@
-from app import db
 
-class user(object):
+class User(object):
 
-    __tablename__ = 'user'
-
-    id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(255))
-    lastname = db.Column(db.String(255))
-    address = db.Column(db.String(255))
-    email = db.Column(db.String(255))
-    password = db.Column(db.String(255))
-
-    def __init__(self, firstname = "", lastname = "", address = "", email = "", password = ""):
+    def __init__(self, firstname = "", lastname = "", email = "", password = ""):
         self.firstname = firstname
         self.lastname = lastname
-        self.address = address
         self.email = email
         self.password = password
-
-    def addUser(self, item, quantity):
-        #Write user details to database
-        db.session.add(self)
-        db.session.commit()
-
-    def editUserAddress(self, newAddress):
-        #Edit address
-        self.address = newAddress
 
     def changePassword(self, newPassword):
         #Change password
         self.password = newPassword
 
-    def viewUserName(self):
+    def getFirstName(self):
         #View user name
-        return (self.firstname + ' ' + self.lastname)
+        return (self.firstname)
 
-    def viewUserAddress(self):
-        #View user address
-        return self.address
+    def getLastName(self):
+        #View user name
+        return (self.lastname)
 
-    def viewUserEmail(self):
+    def getUserEmail(self):
         #View user email
         return self.email
 
